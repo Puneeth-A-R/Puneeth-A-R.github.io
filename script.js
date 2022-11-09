9b9bf9158cd7125c1fe497e10b768076bbbdee80
Puneeth-A-R <89592469+Puneeth-A-R@users.noreply.github.com>
2022-11-09 18:57:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ var loader = new THREE.GLTFLoader();
 var mixer;
 var model;
 loader.load(
-  "https://raw.githubusercontent.com/baronwatts/models/master/robber.glb",
+  "https://github.com/Puneeth-A-R/Puneeth-A-R.github.io/blob/4803c5f4561235eaf01855ca859ceb942d1f54f9/book.glb",
   function (gltf) {
     gltf.scene.traverse(function (node) {
       if (node instanceof THREE.Mesh) {
