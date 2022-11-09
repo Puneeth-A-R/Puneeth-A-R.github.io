6774f85294fb1b9c751ce3f62bc1a861fb972191
Puneeth-A-R <89592469+Puneeth-A-R@users.noreply.github.com>
2022-11-09 19:17:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//==================================================== CORS
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+//==================================================== Beginning of script
+
 console.clear();
 
 //===================================================== canvas
